872591304e9bbc2e87ea2bccf4a47ce7f5601ae6
Miah Clay <miahb88@gmail.com>
2022-05-25 18:33:49 -0400

    factorial solved

--- python/factorial.py
+++ python/factorial.py
@@ -1,2 +1,20 @@
 def factorial(num):
-	# your code here
+	list = []
+	x = num
+	while (x>0):
+		for x in range(num,-1,-1):
+			list.append(x)
+			
+			# print(list)
+	
+	list2 = list[:-1]
+	# print(list2)
+	product = 1
+	for y in list2:
+		product *= y
+	return product
+
+# factorial(8)
+# factorial(18)
+# factorial(45)
+
